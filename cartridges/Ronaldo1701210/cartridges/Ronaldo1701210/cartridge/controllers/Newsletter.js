2d2096e1f533c9ce7541b8dc549e4a1d348c8278
17rodr1gues <82651137+17rodr1gues@users.noreply.github.com>
2022-07-26 15:07:50 +0100

    Finalizado o projeto

--- cartridges/Ronaldo1701210/cartridges/Ronaldo1701210/cartridge/controllers/Newsletter.js
+++ cartridges/Ronaldo1701210/cartridges/Ronaldo1701210/cartridge/controllers/Newsletter.js
@@ -1,94 +1,66 @@
 'use strict';
 
-/**
- * @namespace Newsletter
- */
-
 var server = require('server');
-
+var csrfProtection = require('*/cartridge/scripts/middleware/csrf');
 var URLUtils = require('dw/web/URLUtils');
-var Resource = require('dw/web/Resource');
 
-
-/**
- * Newsletter-Landing : This endpoint is called to load Newsletter landing page
- * @name Base/Newsletter-Landing
- * @function
- * @memberof Newsletter
- * @param {middleware} - server.middleware.https
- * @param {renders} - isml
- * @param {serverfunction} - get
- */
 server.get(
-    'Show', server.middleware.https, function (req, res, next) {
-    var actionUrl = URLUtils.url('Newsletter-Subscribe');
+    'Show',
+    server.middleware.https,
+    csrfProtection.generateToken,
+    function (req, res, next) {
+        var actionUrl = dw.web.URLUtils.url('Newsletter-Handler');
+        var newsletterForm = server.forms.getForm('Newsletter');
+        newsletterForm.clear();
+
+        res.render('/newsletter/newsletter', {
+            actionUrl: actionUrl,
+            newsletterForm: newsletterForm
+        });
+
+        next();
+    }
+);
+
+server.post(
+    'Handler',
+    server.middleware.https,csrfProtection.validateAjaxRequest,
+    function (req, res, next) {
+    	var newsletterForm = server.forms.getForm('Newsletter');
+    	var continueUrl = URLUtils.url('Newsletter-Show');
 
 
-    
-    
-  //  var newsletterform = server.forms.getForm('Newsletter'); //Buscar a variavel
-
-  //  newsletterform.clear();
-
-    res.render('newsletter/newsletter', {
-        actionUrl: actionUrl
-      
-    });
-
-    next();
-});
-
-/**
- * Newsletter-Subscribe : This endpoint is called to submit the shopper's Newsletter information
- * @name Base/Newsletter-Subscribe
- * @function
- * @memberof Newsletter
- * @param {middleware} - server.middleware.https
- * @param {httpparameter} - contactFirstName - First Name of the shopper
- * @param {httpparameter} - contactLastName - Last Name of the shopper
- * @param {httpparameter} - contactEmail - Email of the shopper
- * @param {httpparameter} - contactComment - Comments entered by the shopper
- * @param {returns} - json
- * @param {serverfunction} - post
- */
-server.post('Subscribe', server.middleware.https, function (req, res, next) {
-
-    var newsletterform = server.forms.getForm('Newsletter');
-    var continueUrl = URLUtils.url('Newsletter-Show');
-
-    if (newsletter.valid) {
-        var transation = require('dw/system/Transaction');
-
-        try {
-            transaction.wrap(function () {
-                var customObjectMgr = require('dw/object/CustomObjectMgr'); // criar manage objecto
-                var co = customObjectMgr.createCustomObject('newsletterSign', newsletterform.email.value);
-                co.custom.firstName = newsletterform.fname.value;
-                co.custom.lastName = newsletterform.lname.value;
-                res.render('newsletter/newsletterSucess', {
-                    continueUrl: continueUrl,
-                    newsletterform: newsletterform
-
+    	if (newsletterForm.valid) {
+            var Transaction = require('dw/system/Transaction');
+    		try {
+                Transaction.wrap(function(){
+                    var CustomObjectMgr = require('dw/object/CustomObjectMgr');
+                    var co = CustomObjectMgr.createCustomObject('NewsletterSubscription', newsletterForm.email.value);
+                    co.custom.firstName = newsletterForm.fname.value;
+                    co.custom.lastName = newsletterForm.lname.value;
+                    res.render('/newsletter/newsletterSucess',{
+                        continueUrl: continueUrl,
+                     
+                        newsletterForm: newsletterForm
+                    });
                 });
-
-            });
-
-
-        } catch (error) {
-            var erro = error;
-            res.render('newsletter/newsletterError', {
+            }catch(e){
+                var err = e;
+                res.render('/newsletter/newsletterError', {
+                    errorMsg: "Erro ao Subscrever",
+                    continueUrl: continueUrl
+                });
+            }
+        }else{
+            res.render('/newsletter/newsletterError', {
                 errorMsg: "Erro ao Subscrever",
-                continueUrl: continueUrl
+            continueUrl: continueUrl
             });
         }
-    } else {
-        res.render('newsletter/newsletterError', {
-            errorMsg: "Erro ao Subscrever",
-            continueUrl: continueUrl
-        });
+        next();
     }
+);
 
-    next();
-});
+
 
 module.exports = server.exports();
